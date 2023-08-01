e63bbaaeb63fdadf0e1b63625915fcbd9ba50a1a
giffen-good <chris6rock@gmail.com>
2023-08-01 19:12:28 -0400

    completed insert json row to bq

--- get_orders.py
+++ get_orders.py
@@ -1,73 +1,13 @@
 from dotenv import load_dotenv
-import json
-from util import fetch_shopify_data_since_id
-import os
+from util import fetch_shopify_data_since_id, insert_json_rows_to_bq
 from google.cloud import bigquery as bq
 load_dotenv()
-
-def convert_nested_dict_to_json_string(data: dict):
-    i = 0
-    for key, value in data.items():
-        i += 1
-        if isinstance(value, dict):
-            data[key] = json.dumps(value)
-    print("columns : ", i)
-    return data
-
-def create_insert_statement(table_id: str, data: list[dict]):
-    new_rows = []
-    columns = []
-    first = True
-    for row in data[0:2]:
-        print(row)
-        if first:
-            columns = row.keys()
-            first = False
-        vals = []
-        for key, val in row.items():
-            vals.append(val)
-
-        new_row = f"({','.join(str(val) for val in vals)})"
-        new_rows.append(new_row)
-
-    insert_query = f"""
-        INSERT INTO `no-maintenance`.shopify.{table_id} ({",".join(columns)})
-        VALUES
-            {",".join(new_rows)}
-    """
-    return insert_query
-
-def add_data_to_bq_table(client, table_id: str, data: list[dict]):
-    # Get table reference
-    query = create_insert_statement(table_id, data)
-    print(query)
-
-    table_ref = client.dataset(os.getenv("BIGQUERY_SHOP_DATASET_ID")).table(table_id)
-    # Get table object
-    table = client.get_table(table_ref)
-    job_config = bq.LoadJobConfig()
-    job_config.source_format = bq.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
-    job_config.write_disposition = bq.WriteDisposition.WRITE_APPEND
-    for row in data:
-        convert_nested_dict_to_json_string(row)
-    print(table.schema)
-    sch = table.schema
-    errors = client.insert_rows_json(table, data)
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
-    # job = client.load_table_from_json(data, table_ref, job_config=job_config)  # Make an API request.
-    # print("Loaded {} rows into {}:{}.".format(job.output_rows, table_id))
 
 if __name__ == '__main__':
     params = {
         'status': 'any',
         'limit': 250
     }
-    data = fetch_shopify_data_since_id('orders', '5381186355438', params)
     client = bq.Client(project="no-maintenance")
-    add_data_to_bq_table(client, 'orders_bak', data)
-    print(json)
+    data = fetch_shopify_data_since_id(client, 'orders', None, params)
 
--- util.py
+++ util.py
@@ -1,12 +1,9 @@
 import json
 import os
-import csv
 import re
 import requests
 from dotenv import load_dotenv
-import ast
-
-from google.cloud import bigquery
+from google.cloud import bigquery as bq
 
 load_dotenv()
 
@@ -31,25 +28,13 @@ def has_next_page(response):
     link = response.headers['Link']
     return 'next' in link
 
-
-def write_to_csv(data, filename, field_names, write_header=False, mode='a'):
-    with open(f'data/{filename}.csv', mode, newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=field_names)
-
-        # Write the header row
-        if write_header:
-            writer.writeheader()
-
-        # Write each dictionary as a separate row in the CSV file
-        writer.writerows(data)
-
-
-def fetch_shopify_data_since_id(endpoint: str, since_id: int, params={}):
+def fetch_shopify_data_since_id(client, endpoint: str, since_id: int, params={}):
     headers = {
         "Content-Type": "application/json",
         "X-Shopify-Access-Token": os.getenv('SHOPIFY_ACCESS_TOKEN'),
     }
-    params["since_id"] = since_id
+    if since_id is not None:
+        params["since_id"] = since_id
     response = requests.get(
         f"{os.getenv('SHOPIFY_SHOP_URL')}/admin/api/{os.getenv('SHOPIFY_API_VERSION')}/{endpoint}.json",
         headers=headers, params=params)
@@ -59,87 +44,15 @@ def fetch_shopify_data_since_id(endpoint: str, since_id: int, params={}):
         print('No data to write')
         return
     i = 0
+    insert_json_rows_to_bq(client, endpoint, res)
     while has_next_page(response):
         i += 1
         next_page = get_next_page_link(response)
         print(f"page #{i} - {next_page}")
         response = requests.get(next_page, headers=headers)
         data = response.json()[endpoint]
-        res = [*res, *data]
+        insert_json_rows_to_bq(client, endpoint, data)
     return res
-
-
-def update_shop_table_since_id(client, endpoint: str, since_id: int, bq_table_id: str, params={}):
-    headers = {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": os.getenv('SHOPIFY_ACCESS_TOKEN'),
-    }
-    params["since_id"] = since_id
-    response = requests.get(
-        f"{os.getenv('SHOPIFY_SHOP_URL')}/admin/api/{os.getenv('SHOPIFY_API_VERSION')}/{endpoint}.json",
-        headers=headers, params=params)
-    data = response.json()[endpoint]
-    field_names = data[0].keys()
-    if len(data) == 0:
-        print('No data to write')
-        return
-    write_shop_data_to_bq(client, bq_table_id, data)
-    i = 0
-    while has_next_page(response):
-        i += 1
-        next_page = get_next_page_link(response)
-        print(f"page #{i} - {next_page}")
-        response = requests.get(next_page, headers=headers)
-        data = response.json()[endpoint]
-        write_shop_data_to_bq(client, bq_table_id, data)
-
-
-def write_shop_data_to_bq(client, table_id: str, data: list[dict]):
-    # Get table reference
-    table_ref = client.dataset(os.getenv("BIGQUERY_SHOP_DATASET_ID")).table(table_id)
-    # Get table object
-    table = client.get_table(table_ref)
-    json_str = json.dumps(data)
-    # Assuming the response is in JSON format, this will be a JSON string
-    print(json_str)
-    # Then you can load it into BigQuery as JSON
-    job_config = bigquery.LoadJobConfig()
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job = client.load_table_from_file(json_str, table, job_config=job_config)
-    errors = job.result()
-
-    if errors:
-        print('Errors:')
-        print(errors)
-    else:
-        print('Rows inserted successfully.')
-
-
-def write_local_shopify_csv(endpoint: str, filename: str, params=None, append_to_existing_file: bool = True):
-    headers = {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": os.getenv('SHOPIFY_ACCESS_TOKEN'),
-    }
-    response = requests.get(
-        f"{os.getenv('SHOPIFY_SHOP_URL')}/admin/api/{os.getenv('SHOPIFY_API_VERSION')}/{endpoint}.json",
-        headers=headers, params=params)
-    data = response.json()[endpoint]
-    field_names = data[0].keys()
-    if len(data) == 0:
-        print('No data to write')
-        return
-    if append_to_existing_file:
-        write_to_csv(data, filename, field_names, True, 'a')
-    else:
-        write_to_csv(data, filename, field_names, True, 'w')
-    i = 0
-    while has_next_page(response):
-        i += 1
-        next_page = get_next_page_link(response)
-        print(f"page #{i} - {next_page}")
-        response = requests.get(next_page, headers=headers)
-        write_to_csv(response.json()[endpoint], filename, field_names)
-
 
 def reformat_json_string(json_string: str) -> str:
     # Replace Python None, True, False with JSON null, true, false
@@ -181,3 +94,29 @@ def reformat_json_quotes(json_string: str) -> str:
     pattern = r"(?<![A-Za-z])'(?![A-Za-z])|(?<=[A-Za-z])'(?![A-Za-z])|(?<![A-Za-z])'(?=[A-Za-z])"
     fixed_str = re.sub(pattern, '"', json_string)
     return fixed_str
+
+
+def convert_nested_dict_to_json_string(data: dict):
+    for key, value in data.items():
+        if isinstance(value, dict) or isinstance(value, list):
+            data[key] = json.dumps(value)
+    return data
+
+
+def insert_json_rows_to_bq(client, table_id: str, data: list[dict]):
+    table_ref = client.dataset(os.getenv("BIGQUERY_SHOP_DATASET_ID")).table(table_id)
+    # Get table object
+    table = client.get_table(table_ref)
+    job_config = bq.LoadJobConfig()
+    job_config.source_format = bq.SourceFormat.NEWLINE_DELIMITED_JSON
+    job_config.autodetect = True
+    job_config.write_disposition = bq.WriteDisposition.WRITE_APPEND
+    for row in data:
+        convert_nested_dict_to_json_string(row)
+    errors = client.insert_rows_json(table, data)
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+    # job = client.load_table_from_json(data, table_ref, job_config=job_config)  # Make an API request.
+    # print("Loaded {} rows into {}:{}.".format(job.output_rows, table_id))
